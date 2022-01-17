ff1bd842549d6884aee665977bf28c97de314b4e
Nicklas <gitnicklas@pm.me>
2022-01-17 14:06:16 +0200

    update

--- .bashrc
+++ .bashrc
@@ -34,7 +34,7 @@ alias daymux='~/.dotfiles/tmux/tmux_daily.sh'
 # Uni
 alias course='cd ~/Documents/Study/Bachelor_0/Semester_1/'
 alias mage='g++ main.cpp -Wall -o main.exe'
-alias study='cd ~/Study/Bachelor_2/Semester_1/'
+alias study='cd ~/Study/Bachelor_2/Semester_2/'
 
 # ssh
 if [ -z "$SSH_AUTH_SOCK" ] ; then
@@ -84,17 +84,20 @@ alias dailyssh='ssh -v $DAILYUSERNAME@ssh.eu.pythonanywhere.com'
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/opt/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/usr/etc/profile.d/conda.sh" ]; then
-        . "/usr/etc/profile.d/conda.sh"
+    if [ -f "/opt/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/usr/bin:$PATH"
+        export PATH="/opt/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
 
 
+# BEGIN_KITTY_SHELL_INTEGRATION
+if test -n "$KITTY_INSTALLATION_DIR" -a -e "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; then source "$KITTY_INSTALLATION_DIR/shell-integration/bash/kitty.bash"; fi
+# END_KITTY_SHELL_INTEGRATION
--- tmux/tmux_daily.sh
+++ tmux/tmux_daily.sh
@@ -6,31 +6,31 @@
 session="daily"
 rootdir="~/Projects/daily/daily"
 
-tmux start-server
-
-tmux new-session -d -s $session -n vim
-
-tmux selectp -t 1
-tmux send-keys "cd $rootdir" C-m
-tmux send-keys "vim data_analysis/data_models.py" C-m
-
-tmux splitw -h -p 50
-tmux send-keys "cd $rootdir" C-m
-tmux send-keys "vim data_analysis/views.py" C-m
-
-tmux selectp -t 2
-tmux splitw -v -p 10
-tmux send-keys "cd $rootdir;.." C-m
-tmux send-keys "activate;flask run" C-m
+tmux -u start-server
+     
+tmux -u new-session -d -s $session -n vim
+      
+tmux -u selectp -t 1
+tmux -u send-keys "cd $rootdir" C-m
+tmux -u send-keys "vim data_analysis/data_models.py" C-m
+      
+tmux -u splitw -h -p 50
+tmux -u send-keys "cd $rootdir" C-m
+tmux -u send-keys "vim data_analysis/views.py" C-m
+      
+tmux -u selectp -t 2
+tmux -u splitw -v -p 10
+tmux -u send-keys "cd $rootdir;.." C-m
+tmux -u send-keys "activate;flask run" C-m
 
 # For html
-tmux new-window -t $session:1 -n templates
-tmux send-keys "cd "$rootdir+"/templates" C-m
-tmux splitw -h -p 50
-tmux send-keys "cd "$rootdir+"/templates" C-m
+tmux -u new-window -t $session:1 -n templates
+tmux -u send-keys "cd "$rootdir+"/templates" C-m
+tmux -u splitw -h -p 50
+tmux -u send-keys "cd "$rootdir+"/templates" C-m
 
 # Return to main vim window.
-tmux select-window -t $session:0
+tmux -u select-window -t $session:0
 
 # Attach!
-tmux attach-session -t $session
+tmux -u attach-session -t $session
